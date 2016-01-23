4021846f1aafd5567a7ac2bd903b0443e0c6674e
Will Hartman <william.a.hartman@gmail.com>
2016-01-22 20:42:17 -0500

    Very untested minimax (no pruning or real heuristic)

--- src/main/java/connectn/Player.java
+++ src/main/java/connectn/Player.java
@@ -1,5 +1,7 @@
 package connectn;
 
+import java.util.List;
+
 /**
  * Created by will on 1/21/16.
  */
@@ -34,5 +36,58 @@ public class Player {
                 || board.countRegions(Board.OPPONENT, numWin) > 0
                 || board.getActions(player).size() == 0;
 
+    }
+
+    public Action minimax(Board board, int maxDepth) {
+        List<Action> possibleMoves = board.getActions(Board.PLAYER);
+
+        if(possibleMoves.isEmpty()) {
+            throw new RuntimeException("No possible moves. The game is already a draw!");
+        }
+
+        int currentBestHeuristic = Integer.MIN_VALUE;
+        Action currentBestAction = null;
+        for(Action action: possibleMoves) {
+            Board newBoardState = new Board(board);
+            newBoardState.move(action);
+            int childValue = minimax(newBoardState, Board.OPPONENT, 1, maxDepth);
+
+            if(childValue >= currentBestHeuristic) {
+                currentBestAction = action;
+            }
+        }
+
+        return currentBestAction;
+    }
+
+    private int minimax(Board board, byte currentPlayer, int currentDepth, int maxDepth) {
+        List<Action> possibleMoves = board.getActions(currentPlayer);
+        boolean isMax = currentPlayer == Board.PLAYER;
+
+        //Base case, no moves or at maximum depth
+        //Change "possibleMoves.isEmpty()" to  "board.isTerminating()"
+        if(currentDepth >= maxDepth || possibleMoves.isEmpty()) {
+            return heuristic(board);
+        }
+
+        //Other cases, minimax on all children return the best one
+        int bestState = isMax ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        for(Action action: possibleMoves) {
+            Board newBoardState = new Board(board);
+            newBoardState.move(action);
+            int childValue = minimax(newBoardState, currentPlayer, currentDepth + 1, maxDepth);
+
+            if(isMax && childValue >= bestState) {
+                bestState = childValue;
+            }
+            if(!isMax && childValue <= bestState) {
+                bestState = childValue;
+            }
+        }
+        return bestState;
+    }
+
+    public int heuristic(Board board) {
+        return 0;
     }
 }
